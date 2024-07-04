e248ca1e6cdecd4bc1ba94130a53fa8174955b00
Alez1385 <santiagocaponf@gmail.com>
2024-07-04 15:35:51 -0500

    terminado

--- procesar.php
+++ procesar.php
@@ -3,7 +3,7 @@
 if(isset($_POST)){
     $name = $_POST['name'];
     $email = $_POST['email'];
-    $list = explode(" ", $name);
+    $list = explode(" ", $name); 
     
 }
 ?>
@@ -20,21 +20,22 @@ if(isset($_POST)){
     <div class="container">
         <h2><?= "Bienvenido" ?> </h2>
         <p><?= $name . " tu correo es " . $email?></p>
-        <p><?= strtoupper($name)?></p>
-        <p><?= strtolower($name)?></p>
-        <p><?= trim($name)?></p>
-        <p><?= $list[0] ?></p>
-        <p><?= $list[1] ?></p>
+        <p><?= "Tu nombre en mayusculas es " . strtoupper($name)?></p>
+        <p><?= "Tu nombre en minusculas es " . strtolower($name)?></p>
+        <p><?= "Tu nombre sin espacios es " . trim($name)?></p>
+        <p><?= "Tu nombre es " . $list[0] ?></p>
+        <p><?= "Tu apellido es " . $list[1] ?></p>
         <p><?= "Longitud del nombre: " . strlen($list[0]) ?><br><?=
              "Longitud del apellido: " . strlen($list[1])?></p>
         <?= "hash de tu nombre: " . $hash = md5("Hola Mundo"); ?>
-        <p><?= nl2br("\n" . $name)?></p>
-        <?php if ($mostrarMensaje): ?>
-        <div class="mensaje">Este es un mensaje importante.</div>
+        <p><?= "Tu nombre con salto de linea es " . nl2br("\n" . $name)?></p>
+        <?php if ($name == "Santiago"): ?>
+        <div class="mensaje">Te llamas Santiago.</div>
         <?php else: ?>
-        <div class="sin-mensaje">No hay mensajes.</div>
+        <div class="sin-mensaje">No eres Santiago.</div>
         <?php endif; ?>
-        
+        <p><?="La primera letra de tu nombre es " . substr($list[0], 0, 1) ?></p>
+        <p><?="La primera letra de tu apellido es " . substr($list[1], 0, 1) ?></p>
 
         
     </div>
